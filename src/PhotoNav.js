55a550e93e42ba3b93c6d843be58e8f46b9c0130
spassvogel <exo_00@hotmail.com>
2017-01-13 00:46:03 +0200

    Fixed up  the mess

--- src/PhotoNav.js
+++ src/PhotoNav.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './PhotoNav.css';
 import shuffle from 'shuffle-array';
 
+
 class PhotoNav extends Component {
 	constructor(props) {
 		super(props);
 
 		this.handleClick = this.handleClick.bind(this);
 
-		this.items = shuffle(this.props.photos).map(photo => <li key={photo.thumb}>
-			<img src={ this.props.basePath + "/" + photo.thumb } alt={"Photo by " + photo.author} onClick={this.handleClick}/>
+		this.items = shuffle(this.props.images).map(image => <li key={image.thumb}>
+			<img src={ this.props.basePath + "/" + image.thumb } alt={"Photo by " + image.author} onClick={this.handleClick}/>
 		</li>);
 	}
 
